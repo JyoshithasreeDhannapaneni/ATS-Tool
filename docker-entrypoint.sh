b645f973779bd5aa36c933c0b424d3dcc94e0272
Jyoshitha Dhannapaneni <jyoshitha.dhannapaneni@cloudfuze.com>
2026-02-11 17:48:18 +0530

    Fix docker-entrypoint.sh to properly handle CMD arguments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,5 +8,5 @@ if [ ! -f config.php ] && [ -f config.php.example ]; then
     chmod 644 config.php
 fi
 
-# Start Apache
-exec apache2-foreground
+# Execute the command passed as arguments
+exec "$@"
